8542c97c797151eb13235b267670d38343209628
Johann Weiser <johann.weiser@aon.at>
2017-03-13 10:10:51 +0100

    Hallo1

---
+++ src/weiser/Hallo17.java
@@ -0,0 +1,19 @@
+package weiser;
+
+public class Hallo17 {
+
+	public static void gutenTag() {
+		System.out.println("Guten Tag.");
+	}
+
+	public static void byby() {
+		System.out.println("Auf Wiedersehen.");
+	}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
