7d2372fc0a1a2db8fc9aa3a0e6ee6305ef0e8de0
Johann Weiser <johann.weiser@aon.at>
2017-03-13 10:29:33 +0100

    Mit Punkten.

--- src/weiser/Hallo17.java
+++ src/weiser/Hallo17.java
@@ -3,11 +3,11 @@ package weiser;
 public class Hallo17 {
 
 	public static void gutenTag() {
-		System.out.println("Guten Tag.");
+		System.out.println("Guten Tag....");
 	}
 
 	public static void byby() {
-		System.out.println("Auf Wiedersehen.");
+		System.out.println("Auf Wiedersehen...");
 	}
 
 	
